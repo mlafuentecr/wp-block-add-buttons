766a176aa0af511d9cc29021a2672dbf05061080
mlafuentecr <mlafuente@gmail.com>
2022-01-14 20:23:51 -0600

    example how to extend buttons on block editor

---
+++ src/js/block-text-highlight-button.js
@@ -0,0 +1,49 @@
+// Tiny Button
+wp.blocks.registerBlockStyle('core/button', {
+	name: 'tiny-button',
+	label: 'Tiny',
+});
+wp.blocks.registerBlockStyle('core/button', {
+	name: 'tiny-outline-button',
+	label: 'Tiny Outline',
+});
+
+// Pressable Button
+wp.blocks.registerBlockStyle('core/button', {
+	name: 'pressable-button',
+	label: 'Pressable',
+});
+wp.blocks.registerBlockStyle('core/button', {
+	name: 'pressable-outline-button',
+	label: 'Pressable Outline',
+});
+
+// Plastic Button
+wp.blocks.registerBlockStyle('core/button', {
+	name: 'plastic-button',
+	label: 'Plastic',
+});
+wp.blocks.registerBlockStyle('core/button', {
+	name: 'plastic-outline-button',
+	label: 'Plastic Outline',
+});
+
+// Minimal Button
+wp.blocks.registerBlockStyle('core/button', {
+	name: 'minimal-button',
+	label: 'Minimal',
+});
+wp.blocks.registerBlockStyle('core/button', {
+	name: 'minimal-outline-button',
+	label: 'Minimal Outline',
+});
+
+// Small Button
+wp.blocks.registerBlockStyle('core/button', {
+	name: 'small-button',
+	label: 'Small',
+});
+wp.blocks.registerBlockStyle('core/button', {
+	name: 'small-outline-button',
+	label: 'Small Outline',
+});
