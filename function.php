618aa8360bc1948adb0c0ead9bff2a5ad1f1f8aa
mario Lafuente <45186223+mlafuentecr@users.noreply.github.com>
2022-01-21 16:01:09 -0600

    Update function.php

--- function.php
+++ function.php
@@ -4,7 +4,6 @@
 // Enqueue JS/CSS for the buttons block (front-end)
 function add_block_btns() {
   $blockPath = '/src/js/block-text-highlight-button.js';
-  $blockCssPath = '/src/css/block-text-highlight-button.css';
   wp_enqueue_script('block-buttons', get_template_directory_uri() . $blockPath, ['wp-blocks'], '1.0' , true);
   //wp_enqueue_style('mdlui-buttons',  get_template_directory_uri() . $blockCssPath, [], '1.0');
 }
@@ -14,7 +13,7 @@ add_action('enqueue_block_assets', 'add_block_btns');
 // Enqueue block styles for editor (back-end)
 function add_css_editor() {
   $blockCssPath = '/src/css/block-text-highlight-button.css';
-  wp_enqueue_style('block-buttons', get_template_directory_uri() . $blockPath, __FILE__);
+  wp_enqueue_style('block-buttons', get_template_directory_uri() . $blockCssPath, __FILE__);
 }
 
 add_action('enqueue_block_editor_assets', 'add_css_editor');
